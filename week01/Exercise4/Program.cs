df7185cc3cb3285b4492ab9961f0a9e135254ec5
JonaIMV <jonaimv@gmail.com>
2025-07-08 15:42:23 -0500

    sort numbers added

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,81 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+
+        Console.WriteLine("Please enter a list of numbers, type 0 to end the list:");
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out userNumber))
+            {
+                if (userNumber != 0)
+                {
+                    numbers.Add(userNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
+        int minPositive = int.MaxValue;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < minPositive)
+            {
+                minPositive = number;
+            }
+        }
+
+        if (minPositive != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {minPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers in the list.");
+        }
+           numbers.Sort();
+
+
+        Console.WriteLine("The sorted list is:");
+
+        foreach (int number in numbers)
+        {
+        Console.WriteLine(number);
+        }
     }
 }
