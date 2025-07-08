41440a731aa960073783aba6049648fd07590412
JonaIMV <jonaimv@gmail.com>
2025-07-08 16:30:27 -0500

    function was not valid, already fixed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PropmptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+
+        DisplayResult(name, squared);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Square Number Program!");
+    }
+    static string PropmptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        int number;
+        bool IsValid = false;
+
+        do
+        {
+            Console.Write("What is your favorite number?: ");
+            string input = Console.ReadLine();
+            IsValid = int.TryParse(input, out  number);
+
+            if (!IsValid)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        } while (!IsValid);
+        return number;
+
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string name, int squared)
+    {
+        Console.WriteLine($"Hello {name},the square of your favorite number is {squared}.");
+
     }
 }
