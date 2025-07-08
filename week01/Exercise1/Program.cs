6c864b6c998640f34656b8939ec5c79d86ba1108
JonaIMV <jonaimv@gmail.com>
2025-07-07 19:32:27 -0500

    first try c#

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        AskName();
+        AskAgeAndLocation();
+        AskFoodAndMovies();
+    }
+    static void AskName()
+    {
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your names is {lastName}, {firstName} {lastName}.");
+    }
+    static void AskAgeAndLocation()
+    {
+
+        Console.Write("How old are you? ");
+        string age = Console.ReadLine();
+        Console.Write("Where are you living? ");
+        string location = Console.ReadLine();
+
+        Console.WriteLine($"You are {age} years old and you live in {location}.");
+    }
+    static void AskFoodAndMovies()
+    {
+        Console.Write("What is your favorite food? ");
+        string food = Console.ReadLine();
+        Console.Write("What is your favorite movie? ");
+        string movie = Console.ReadLine();
+        Console.Write("How many times have you watched it? ");
+        int timesWatched = int.Parse(Console.ReadLine());
+
+        if (timesWatched == 1)
+
+        {
+            Console.WriteLine($"Your favorite food is {food}, your favorite movie is {movie}, and you have watched it {timesWatched} time.");
+
+        }
+        else
+        {
+            Console.WriteLine($"Your favorite food is {food}, your favorite movie is {movie}, and you have watched it {timesWatched} times.");
+        }
+       
     }
 }
+
+   
+  
