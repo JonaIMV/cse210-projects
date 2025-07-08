fb7bdda5635f2322ecbf6db09c0df7bfc5cde82e
JonaIMV <jonaimv@gmail.com>
2025-07-07 20:13:35 -0500

    change the location to avoid error reading

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string letterGrade;
+        string sign = "";
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        int lastDigit = gradePercentage % 10;
+        
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        if (letterGrade == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        if (letterGrade == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}{sign}.");
+        
+        if(gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass the course. Please try again.");
+        }
     }
 }
