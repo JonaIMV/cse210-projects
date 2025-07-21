9e24c5ff24501c3a463e417318541b567a480618
JonaIMV <jonaimv@gmail.com>
2025-07-21 10:46:33 -0500

    resume and job cs added

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Tech Solutions";
+        job1._startYear = "2020";
+        job1._endYear = "2025";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data Analyst";
+        job2._company = "Data Insights";
+        job2._startYear = "2025";
+        job2._endYear = "Present";
+
+        Resume myResume = new Resume();
+        myResume._name = "Jonathan Morales";
+
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
