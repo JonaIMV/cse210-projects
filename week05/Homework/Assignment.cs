c7937047dcae5e3e745e765d07b5e1559caf7c1e
JonaIMV <jonaimv@gmail.com>
2025-08-11 19:40:32 -0500

    homework

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,24 @@
+namespace Homework
+{
+    public class Assignment
+    {
+        private string _studentName;
+        private string _topic;
+
+        public Assignment(string studentName, string topic)
+        {
+            _studentName = studentName;
+            _topic = topic;
+        }
+
+        
+        public string StudentName => _studentName;
+        public string Topic => _topic;
+
+        
+        public string GetSummary()
+        {
+            return $"{StudentName} - {Topic}";
+        }
+    }
+}
